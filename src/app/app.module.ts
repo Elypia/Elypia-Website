5daf632b5cae20168cc7b8367a896015121a416d
Mati Tymek <proliental@wp.pl>
2018-06-02 22:18:20 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,16 @@ import { NavbarComponent } from './layout/Navbar/Navbar.component'
 import { NewsListComponent } from './layout/NewsList/NewsList.component'
 import { FooterComponent } from './layout/Footer/Footer.component'
 import { NewsComponent } from './components/News/News.component'
-import { DiscordComponent } from './components/Discord/Discord.component'
+import { DiscordComponent } from './layout/Discord/Discord.component'
 import { LogoComponent } from './components/Logo/Logo.component'
+import { LoginComponent } from './components/LoginForm/LoginForm.component'
 import { BoobsComponent } from './components/Boobs/Boobs.component'
 import { DropdownComponent } from './components/Dropdown/Dropdown.component'
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
     NavbarComponent,
     DiscordComponent,
     NewsListComponent,
---
+++ src/app/components/LoginForm/LoginForm.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'login-form',
+  templateUrl: './LoginForm.component.html',
+  styleUrls: [ './LoginForm.component.css' ]
+})
+export class LoginComponent {
+  expanded: boolean
+}
