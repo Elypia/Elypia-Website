624233591e083e13f24033da92bc86081a614013
Seth <seth@elypia.org>
2019-10-16 05:03:46 +0200

    * Added i18n support.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -58,6 +58,9 @@ import {ProjectComponent} from './components/project/project.component';
 import {CarouselModule} from './carousel/carousel.module';
 import {DonateModule} from './donate/donate.module';
 import {ArticleModule} from './article/article.module';
+import {registerLocaleData} from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+import localeNl from '@angular/common/locales/nl';
 
 const appRoutes: Routes = [
   /** Useless without API */
@@ -167,6 +170,8 @@ const markdownOptions: MarkdownModuleConfig = {
 export class AppModule {
 
   constructor(overlayContainer: OverlayContainer) {
+    registerLocaleData(localeFr);
+    registerLocaleData(localeNl);
     overlayContainer.getContainerElement().classList.add('elypia-light-theme');
   }
 }
