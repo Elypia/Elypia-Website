3a46a93bbda8b42bd595fcb470c297eaa03542e2
Seth Falco <seth@elypia.org>
2020-03-14 13:22:00 +0100

    Updated nginx-ingress and cert-manager versions

--- kubernetes-init.sh
+++ kubernetes-init.sh
@@ -12,16 +12,12 @@ helm repo update
 
 # Install nginx-ingress
 # https://cloud.google.com/community/tutorials/nginx-ingress-gke#deploy_nginx_ingress_controller_with_rbac_enabled
-helm install stable/nginx-ingress --name nginx-ingress --set rbac.create=true --set controller.publishService.enabled=true
+helm install --name nginx-ingress stable/nginx-ingress --set rbac.create=true --set controller.publishService.enabled=true
 
 # Install cert-manager
 # https://cert-manager.io/docs/installation/kubernetes/
-
-# Google Kubernetes Engine Specific - Configure Self as Cluster Admin
-kubectl create clusterrolebinding cluster-admin-binding --clusterrole cluster-admin --user $(gcloud config get-value core/account)
-
 kubectl create namespace cert-manager
-kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.12/deploy/manifests/00-crds.yaml --validate=false
+kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.14.0/cert-manager.crds.yaml
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
-helm install jetstack/cert-manager --version v0.12.0 --name cert-manager --namespace cert-manager
+helm install cert-manager jetstack/cert-manager --namespace cert-manager --version v0.14.0
